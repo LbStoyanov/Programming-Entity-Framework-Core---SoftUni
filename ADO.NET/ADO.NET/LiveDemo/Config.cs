8d7454f6ff54ee94e2eed7504672f6eefa966eed
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-16 21:21:57 +0300

    ADO.NET

---
+++ ADO.NET/ADO.NET/LiveDemo/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveDemo
+{
+    public static class Config
+    {
+        public static string ConnectionString = @"server=DESKTOP-EH9QDAI;Database=Softuni;Integrated Security=True";
+    }
+}
---
+++ ADO.NET/ADO.NET/LiveDemo/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace LiveDemo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            
+            //Created connection
+            using SqlConnection sqlConnection = new SqlConnection(Config.ConnectionString);
+            //Open connection
+            sqlConnection.Open();
+            //Connected
+
+
+
+            //Just for debugging
+            Console.WriteLine("Connection completed!");
+            Console.WriteLine("Press any key to continue!");
+
+
+        }
+    }
+}
