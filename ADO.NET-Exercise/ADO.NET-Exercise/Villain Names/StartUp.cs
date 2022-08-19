8d1dd773da4b8b78b8b6361f548c535bde7b5979
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-19 11:53:26 +0300

    ADO.NET manipulations

---
+++ ADO.NET-Exercise/ADO.NET-Exercise/Villain Names/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Villain_Names
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            using SqlConnection sqlConnection = new SqlConnection(Config.ConnectionString);
+
+            sqlConnection.Open();
+
+            Console.WriteLine("Connected successfully");
+
+            sqlConnection.Close();
+        }
+    }
+}
