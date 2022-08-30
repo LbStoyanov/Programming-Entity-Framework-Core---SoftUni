04965b33e5f2f46525abed7084cceb741d1df9fa
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-08-30 12:46:21 +0300

    EFCore

--- Entity Framework Intro-Exercise/EntityFrameworkIntroExercise/StartUp.cs
+++ Entity Framework Intro-Exercise/EntityFrameworkIntroExercise/StartUp.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using SoftUni.Data;
+﻿using SoftUni.Data;
 using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace SoftUni
 {
