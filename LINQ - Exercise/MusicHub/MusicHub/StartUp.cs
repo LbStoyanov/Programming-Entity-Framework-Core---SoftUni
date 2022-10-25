c939e560744da2e8d3bce1abaa8f3e1c74b86003
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-25 21:55:58 +0300

    CodeFirst

--- LINQ - Exercise/MusicHub/MusicHub/StartUp.cs
+++ LINQ - Exercise/MusicHub/MusicHub/StartUp.cs
@@ -1,12 +1,31 @@
-﻿using System;
-
-namespace MusicHub
+﻿namespace MusicHub
 {
+    using System;
+
+    using Data;
+    using Initializer;
+    using MusicHub.Data;
+
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            MusicHubDbContext context =
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            //Test your solutions here
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
         }
     }
 }
