d03544f83bb0455ccb72533b01748fca4eeae58d
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-27 16:45:59 +0300

    TASK-01-Age Restriction

--- Advanced Querying/BookShop/StartUp.cs
+++ Advanced Querying/BookShop/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
     using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@
         public static void Main()
         {
             //TASK-01-Age Restriction:
+
             //Return in a single string all book titles, each on a new line, that have an age restriction, equal to 
             //the given command. Order the titles alphabetically.
             //Read input from the console in your main method and call your method with the necessary arguments.
@@ -20,35 +22,44 @@
 
 
             using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+            //DbInitializer.ResetDatabase(db);
 
-            string command = Console.ReadLine().ToLower();
-
+            string command = Console.ReadLine();
             var result = GetBooksByAgeRestriction(db, command);
+
+
+            //var result = GetGoldenBooks(db);
 
             Console.WriteLine(result);
         }
 
-        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        public static string GetGoldenBooks(BookShopContext context)
         {
             StringBuilder result = new StringBuilder();
-
-            var books = context
-                               .Books
-                               .Include(b=>b)
-                               .ToList()
-                               .Where(b => b.AgeRestriction.ToString().ToLower() == command)
-                               .OrderBy(b => b.Title)
-                               .Select(b=>b.Title)                             
-                               .ToList();
-
-            foreach (var book in books)
-            {
-                result.AppendLine(book);
-            }
-
+           
 
             return result.ToString().TrimEnd();
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            AgeRestriction ageRestriction;
+
+            bool isParsed = AgeRestriction.TryParse(command,true, out ageRestriction);
+            if (!isParsed)
+            {
+                return String.Empty;
+            }
+
+            var bookTitles = context
+                               .Books
+                               .Where(b => b.AgeRestriction == ageRestriction)
+                               .Select(b => b.Title)
+                               .OrderBy(t => t)                            
+                               .ToList();
+
+
+            return string.Join(Environment.NewLine, bookTitles);
         }
     }
 }
