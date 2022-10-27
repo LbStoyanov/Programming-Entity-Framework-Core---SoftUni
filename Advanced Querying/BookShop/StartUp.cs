e8445d2573cb5aa461a39d14426b37dd4a83d9d4
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-27 15:26:06 +0300

    AgeRestrictionTask

--- Advanced Querying/BookShop/StartUp.cs
+++ Advanced Querying/BookShop/StartUp.cs
@@ -2,6 +2,10 @@
 {
     using Data;
     using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Text;
 
     public class StartUp
     {
@@ -9,6 +13,36 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
+
+            string command = Console.ReadLine().ToLower();
+
+            var result = GetBooksByAgeRestriction(db, command);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var books = context
+                               .Books
+                               .Include(b=>b)
+                               .ToList()
+                               .Where(b => b.AgeRestriction.ToString().ToLower() == command)
+                               .OrderBy(b => b.Title)
+                               .Select(b=>b.Title)                             
+                               .ToList();
+
+            foreach (var book in books)
+            {
+                result.AppendLine(book);
+            }
+
+
+            return result.ToString().TrimEnd();
         }
     }
 }
+
+
