6c383e34a005d8f20f8c4354176294ae0ff319e8
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-03-28 18:11:41 +0300

    ExamPrepPart1

---
+++ ExamPrep/Exam1/Trucks/Data/Models/Truck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.Data.Models
+{
+    public class Truck
+    {
+        public Truck()
+        {
+            this.ClientTrucks = new HashSet<ClientTruck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public string RegistrationNumber  { get; set; }
+
+        [Required]
+        public string VinNumber { get; set; }
+
+        [Required]
+        public int TankCapacity  { get; set;}
+
+        [Required]
+        public int CargoCapacity { get; set; }
+
+        [Required]
+        public CategoryType CategoryType {get; set; }
+
+        [Required]
+        public MakeType MakeType { get; set; }
+
+        
+        [ForeignKey(nameof(Despatcher))]
+        public int DespatcherId { get; set; }
+
+
+        public virtual Despatcher Despatcher { get; set; } = null!;
+
+        public ICollection<ClientTruck> ClientTrucks { get; set; }
+    }
+}
