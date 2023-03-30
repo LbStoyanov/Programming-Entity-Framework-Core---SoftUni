64f999a71b673f937392edf6b79abd22641e84a2
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-03-30 21:23:53 +0300

    ExamPrep

--- ExamPrep/Exam1/Trucks/DataProcessor/Deserializer.cs
+++ ExamPrep/Exam1/Trucks/DataProcessor/Deserializer.cs
@@ -1,56 +1,49 @@
-﻿using System.Text;
-using Newtonsoft.Json;
-using Trucks.Data.Models;
-using Trucks.Data.Models.Enums;
-using Trucks.DataProcessor.ImportDto;
-using Trucks.Utilities;
-
-namespace Trucks.DataProcessor
+﻿namespace Trucks.DataProcessor
 {
     using System.ComponentModel.DataAnnotations;
-    using Data;
+    using System.Text;
 
+    using Newtonsoft.Json;
+
+    using Data;
+    using Data.Models;
+    using Data.Models.Enums;
+    using ImportDto;
+    using Utilities;
 
     public class Deserializer
     {
         private const string ErrorMessage = "Invalid data!";
-
         private const string SuccessfullyImportedDespatcher
             = "Successfully imported despatcher - {0} with {1} trucks.";
-
         private const string SuccessfullyImportedClient
             = "Successfully imported client - {0} with {1} trucks.";
 
-        private static XmlHelper XmlHelper;
-
-        
+        private static XmlHelper xmlHelper;
 
         public static string ImportDespatcher(TrucksContext context, string xmlString)
         {
-            StringBuilder output = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
+            xmlHelper = new XmlHelper();
 
-            XmlHelper = new XmlHelper();
+            ImportDespatcherDto[] despatcherDtos =
+                xmlHelper.Deserialize<ImportDespatcherDto[]>(xmlString, "Despatchers");
 
-            ImportDespatcherDto[] despatcherDtoS =
-                XmlHelper.Deserialize<ImportDespatcherDto[]>(xmlString, "Despatchers");
-
-            ICollection<Despatcher> validDespatchers = new HashSet<Despatcher>();
-
-            foreach (ImportDespatcherDto despatcherDto in despatcherDtoS)
+            ICollection<Despatcher> validDespatcher = new HashSet<Despatcher>();
+            foreach (ImportDespatcherDto despatcherDto in despatcherDtos)
             {
                 if (!IsValid(despatcherDto))
                 {
-                    output.AppendLine(ErrorMessage);
+                    sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
                 ICollection<Truck> validTrucks = new HashSet<Truck>();
-
                 foreach (ImportTruckDto truckDto in despatcherDto.Trucks)
                 {
                     if (!IsValid(truckDto))
                     {
-                        output.AppendLine(ErrorMessage);
+                        sb.AppendLine(ErrorMessage);
                         continue;
                     }
 
@@ -61,10 +54,8 @@ namespace Trucks.DataProcessor
                         TankCapacity = truckDto.TankCapacity,
                         CargoCapacity = truckDto.CargoCapacity,
                         CategoryType = (CategoryType)truckDto.CategoryType,
-                        MakeType = (MakeType)truckDto.MakeType,
-
+                        MakeType = (MakeType)truckDto.MakeType
                     };
-
                     validTrucks.Add(truck);
                 }
 
@@ -74,41 +65,39 @@ namespace Trucks.DataProcessor
                     Position = despatcherDto.Position,
                     Trucks = validTrucks
                 };
-
-                validDespatchers.Add(despatcher);
-                output.AppendLine(string.Format(
-                    $"Successfully imported despatcher – {despatcher.Name} with {validTrucks.Count} trucks."));
+                validDespatcher.Add(despatcher);
+                sb
+                    .AppendLine(String.Format(SuccessfullyImportedDespatcher, despatcher.Name, validTrucks.Count));
             }
 
-            context.Despatchers.AddRange(validDespatchers);
+            context.Despatchers.AddRange(validDespatcher);
             context.SaveChanges();
 
-            return output.ToString().TrimEnd();
-
+            return sb.ToString().TrimEnd();
         }
+
         public static string ImportClient(TrucksContext context, string jsonString)
         {
-            StringBuilder output = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
 
-            ImportClientDto[] clientDtos = JsonConvert.DeserializeObject<ImportClientDto[]>(jsonString);
+            ImportClientDto[] clientDtos =
+                JsonConvert.DeserializeObject<ImportClientDto[]>(jsonString);
 
             ICollection<Client> validClients = new HashSet<Client>();
-
             ICollection<int> existingTruckIds = context.Trucks
-                .Select(x => x.Id)
-                .ToList();
-
+                .Select(t => t.Id)
+                .ToArray();
             foreach (ImportClientDto clientDto in clientDtos)
             {
                 if (!IsValid(clientDto))
                 {
-                    output.AppendLine(ErrorMessage);
+                    sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
                 if (clientDto.Type == "usual")
                 {
-                    output.AppendLine(ErrorMessage);
+                    sb.AppendLine(ErrorMessage);
                     continue;
                 }
 
@@ -123,29 +112,27 @@ namespace Trucks.DataProcessor
                 {
                     if (!existingTruckIds.Contains(truckId))
                     {
-                        output.AppendLine(ErrorMessage);
+                        sb.AppendLine(ErrorMessage);
                         continue;
                     }
 
-                    ClientTruck clientTruck = new ClientTruck()
+                    ClientTruck ct = new ClientTruck()
                     {
                         Client = client,
                         TruckId = truckId
                     };
-
-                    client.ClientsTrucks.Add(clientTruck);
+                    client.ClientsTrucks.Add(ct);
                 }
 
                 validClients.Add(client);
-                output.AppendLine(string.Format(
-                    $"Successfully imported client – {client.Name} with {client.ClientsTrucks.Count} trucks."));
+                sb
+                    .AppendLine(String.Format(SuccessfullyImportedClient, client.Name, client.ClientsTrucks.Count));
             }
-
 
             context.Clients.AddRange(validClients);
             context.SaveChanges();
 
-            return output.ToString().TrimEnd();
+            return sb.ToString().TrimEnd();
         }
 
         private static bool IsValid(object dto)
