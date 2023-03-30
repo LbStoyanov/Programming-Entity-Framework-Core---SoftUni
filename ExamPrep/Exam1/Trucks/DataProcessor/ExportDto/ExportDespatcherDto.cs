7df001cee23cac66911ca12cd25fe03f33a14c57
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-03-30 21:30:03 +0300

    ExamPrep

---
+++ ExamPrep/Exam1/Trucks/DataProcessor/ExportDto/ExportDespatcherDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Trucks.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+    using Trucks.DataProcessor.ImportDto;
+
+    [XmlType("Despatcher")]
+    public class ExportDespatcherDto
+    {
+        [XmlElement("DespatcherName")]
+        public string DespatcherName { get; set; } = null!;
+
+        [XmlAttribute("TrucksCount")]
+        public int TrucksCount { get; set; }
+
+        [XmlArray("Trucks")]
+        public ExportTruckDto[] Trucks { get; set; } = null!;
+    }
+}
