7f153ad882a7c747ab37a919f620994185c27ae7
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-03-30 20:31:06 +0300

    ExamPrep

---
+++ ExamPrep/Exam1/Trucks/DataProcessor/ImportDto/ImportClientDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Trucks.Data.Models;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+    public class ImportClientDto
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; } = null!;
+
+        [JsonProperty("Nationality")]
+        public string Nationality { get; set; } = null!;
+
+        [JsonProperty("Type")]
+        public string Type { get; set; } = null!;
+
+        [JsonProperty("ClientsTrucks")]
+        public ICollection<ClientTruck> ClientsTrucks { get; set; }
+    }
+}
