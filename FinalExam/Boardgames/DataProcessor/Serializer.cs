ddecb56c0d4e1b47fa1714426d5af5c831269952
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-04-01 17:57:26 +0300

    FinalExam

--- FinalExam/Boardgames/DataProcessor/Serializer.cs
+++ FinalExam/Boardgames/DataProcessor/Serializer.cs
@@ -1,4 +1,7 @@
-﻿namespace Boardgames.DataProcessor
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Boardgames.DataProcessor
 {
     using Boardgames.Data;
 
@@ -11,7 +14,32 @@
 
         public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
         {
-            throw new NotImplementedException();
+            var sellers = context.Sellers
+                .Include(c => c.BoardgamesSellers)
+                .ThenInclude(x => x.Boardgame)
+                .AsNoTracking()
+                .ToArray()
+                .Where(x => x.BoardgamesSellers.Any(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating))
+                .Select(x => new
+                {
+                    Boardgames = x.BoardgamesSellers
+                        .Where(y => y.Boardgame.YearPublished >= year && y.Boardgame.Rating <= rating)
+                        .Select(bg => new
+                        {
+                            Name =bg.Boardgame.Name,
+                            Rating = bg.Boardgame.Rating,
+                            Mechanics = bg.Boardgame.Mechanics,
+                            CategoryType = bg.Boardgame.CategoryType.ToString()
+                        })
+                        .OrderByDescending(z => z.Rating)
+                        .ThenBy(z => z.Name)
+                })
+                .OrderByDescending(x => x.Boardgames.Count())
+                .ThenBy(x => x.Boardgames.Select(y => y.Name))
+                .Take(5)
+                .ToList();
+
+            return JsonConvert.SerializeObject(sellers, Formatting.Indented);
         }
     }
 }
