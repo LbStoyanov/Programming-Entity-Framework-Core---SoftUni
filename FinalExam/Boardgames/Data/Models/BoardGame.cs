ac7965a9712ffa50a8c84ef501bc62257db30582
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-04-01 14:15:46 +0300

    FinalExam

---
+++ FinalExam/Boardgames/Data/Models/BoardGame.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.Data.Models
+{
+    public class BoardGame
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public double Rating { get; set; }
+
+        [Required]
+        public int YearPublished { get; set; }
+    }
+}
