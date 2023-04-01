77dfbe156cff0eb65b67d51c7c5a02609c22d5f3
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-04-01 15:50:23 +0300

    FinalExam

---
+++ FinalExam/Boardgames/DataProcessor/ImportDto/ImportCreatorDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    [XmlType("Creator")]
+    public class ImportCreatorDto
+    {
+        [XmlElement("FirstName")]
+        [Required]
+        [Range(2,7)]
+        public string FirstName { get; set; } = null!;
+
+        [XmlElement("LastName")]
+        [Range(2, 7)]
+        public string LastName { get; set; } = null!;
+
+        [XmlArray("Boardgames")]
+        public ImportBoardgameDto[] Boardgames { get; set; } = null!;
+    }
+}
